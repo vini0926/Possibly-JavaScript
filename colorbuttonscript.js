457da14bd2dcbb5ae1e307aa0b0fb1761350763d
vini0926 <viniciusgdl09@gmail.com>
2026-05-11 22:12:52 -0300

    color button thing

---
+++ colorbuttonscript.js
@@ -0,0 +1,19 @@
+function colorChanger() {
+    const div1 = document.getElementById("div-1");
+    const div2 = document.getElementById("div-2");
+    const div3 = document.getElementById("div-3");
+
+    div1.style.backgroundColor = "red";
+    div2.style.backgroundColor = "lightGreen";
+    div3.style.backgroundColor = "lightBlue";
+}
+
+function colorReverter() {
+    const div1 = document.getElementById("div-1");
+    const div2 = document.getElementById("div-2");
+    const div3 = document.getElementById("div-3");
+
+    div1.style.backgroundColor = "";
+    div2.style.backgroundColor = "";
+    div3.style.backgroundColor = "";
+}
